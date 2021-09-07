633ecc4e6c483165fa0fc4afa8a3d14e046e4dd0
Bruno Coronado <bcoronado.morales@gmail.com>
2021-09-07 14:49:28 -0600

    V1 Modelo Proyecto 1

---
+++ Proyecto 1/API/scripts/modelo.sql
@@ -0,0 +1,15 @@
+CREATE TABLE public.usuario (
+	id_usuario SERIAL PRIMARY KEY,
+	usuario VARCHAR(50) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    ubicacion VARCHAR(250) NOT NULL,
+);
+
+CREATE TABLE public.medicion (
+	id_medicion SERIAL PRIMARY KEY,
+	peso DECIMAL NOT NULL,
+    id_usuario INTEGER NOT NULL,
+    fecha_hora_inicio TIMESTAMP NOT NULL,
+    fecha_hora_fin TIMESTAMP NULL,
+    CONSTRAINT fk_medicion_usuario FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
+);
